44774bf9c4283e18c865f54265933058c1326674
JIBIN N <113666793+jibinntcr@users.noreply.github.com>
2023-11-13 09:29:36 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-# Display usage information
-echo "Usage: ./port-scanner.sh [IP] [PORT_RANGE]"
-echo
+# Function to display script usage
+usage() {
+    echo "Usage: $0 -i <IP_address> -p <max_port_range>"
+    exit 1
+}
 
-# Check if an IP address is provided as an argument
-if [ "$1" = "" ]; then
-  echo "Error: Please provide an IP address."
-  echo "Usage: ./port-scanner.sh [IP] [PORT_RANGE]"
-  exit 1
+# Function to check if a port is open
+is_port_open() {
+    (echo > /dev/tcp/$ip/$1) > /dev/null 2>&1
+}
+
+# Set default values
+ip=""
+ports=""
+
+# Parse command line arguments using getopts
+while getopts "i:p:" opt; do
+    case $opt in
+        i)
+            ip="$OPTARG"
+            ;;
+        p)
+            ports="$OPTARG"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Check if the required options are provided
+if [[ -z $ip || -z $ports ]]; then
+    usage
 fi
 
-# Set a default port range or allow the user to provide it as an argument
-PORT_RANGE=${2:-1-65535}
-
-# Inform the user that port scanning is in progress
-echo "Please wait while it is scanning all the open ports..."
-
-# Use netcat to scan for open ports in the specified range on the specified IP address
-nc -nvz $1 $PORT_RANGE > $1.txt 2>&1
-
-# Sort the output numerically and display it
-sort -n $1.txt
-
-# Remove the temporary file created during the scan
-rm -rf $1.txt
+# Scan and display open ports
+for ((port = 1; port <= ports; port++)); do
+    # Check if the port is open using the is_port_open function
+    if is_port_open "$port"; then
+        echo "$ip:$port is open"
+    fi
+done
