85a3dc4738d55f0419a127c2c39e70dcd3a011e6
JIBIN N <113666793+jibinntcr@users.noreply.github.com>
2023-11-13 09:44:54 +0530

    portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-# Function to display script usage
-usage() {
-    echo "Usage: $0 -i <IP_address> -p <max_port_range>"
+# Port Scanner Script
+
+# Display script usage
+show_usage() {
+    echo "Usage: $0 -h <host_address> -r <max_port_range>"
     exit 1
 }
 
-# Function to check if a port is open
+# Check if a port is open
 is_port_open() {
-    (echo > /dev/tcp/$ip/$1) > /dev/null 2>&1
+    (echo > /dev/tcp/$host/$1) > /dev/null 2>&1
 }
 
 # Set default values
-ip=""
-ports=""
+host=""
+max_ports=""
 
-# Parse command line arguments using getopts
-while getopts "i:p:" opt; do
-    case $opt in
-        i)
-            ip="$OPTARG"
+# Parse command-line arguments using getopts
+while getopts "h:r:" option; do
+    case $option in
+        h)
+            host="$OPTARG"
             ;;
-        p)
-            ports="$OPTARG"
+        r)
+            max_ports="$OPTARG"
             ;;
         *)
-            usage
+            show_usage
             ;;
     esac
 done
 
 # Check if the required options are provided
-if [[ -z $ip || -z $ports ]]; then
-    usage
+if [[ -z $host || -z $max_ports ]]; then
+    show_usage
 fi
 
 # Scan and display open ports
-for ((port = 1; port <= ports; port++)); do
+for ((port_number = 1; port_number <= max_ports; port_number++)); do
     # Check if the port is open using the is_port_open function
-    if is_port_open "$port"; then
-        echo "$ip:$port is open"
+    if is_port_open "$port_number"; then
+        echo "$host:$port_number is open"
     fi
 done
